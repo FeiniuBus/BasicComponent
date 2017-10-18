61ec5ef0a77a6c8155f6d19fcf872c16b545ef03
徐乔伦 <xql@feiniubus.com>
2017-10-18 18:21:36 +0800

    添加json序列化框架

---
+++ src/FeiniuBus.AspNetCore.Json/Converters/IdConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FeiniuBus.AspNetCore.Json.Converters
+{
+    public class IdConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value.ToString());
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jt = JToken.ReadFrom(reader);
+            if (objectType == typeof(long))
+            {
+                return jt.Value<long>();
+            }
+            if (objectType == typeof(ulong))
+            {
+                return jt.Value<ulong>();
+            }
+            if (objectType == typeof(long?))
+            {
+                return jt.Value<long?>();
+            }
+
+            return jt.Value<ulong?>();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(long) == objectType || typeof(long?) == objectType || typeof(ulong) == objectType ||
+                   typeof(ulong?) == objectType;
+        }
+    }
+}
---
+++ src/FeiniuBus.AspNetCore.Json/FeiniuBusJsonConvert.cs
@@ -0,0 +1,46 @@
+﻿using FeiniuBus.AspNetCore.Json.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FeiniuBus.AspNetCore.Json
+{
+    public static class FeiniuBusJsonConvert
+    {
+        private static readonly JsonSerializerSettings Settings;
+
+        static FeiniuBusJsonConvert()
+        {
+            Settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateFormatString = "yyyy'-'MM'-'dd' 'HH':'mm':'ss",
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+            
+            Settings.Converters.Add(new StringEnumConverter());
+            Settings.Converters.Add(new IdConverter());
+        }
+
+        public static string SerializeObject(object value)
+        {
+            return JsonConvert.SerializeObject(value, Settings);
+        }
+
+        public static string SerializeObject(object value, JsonSerializerSettings settings)
+        {
+            return JsonConvert.SerializeObject(value, settings);
+        }
+
+        public static T DeserializeObject<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        public static T DeserializeObject<T>(string value, JsonSerializerSettings settings)
+        {
+            return JsonConvert.DeserializeObject<T>(value, settings);
+        }
+    }
+}
