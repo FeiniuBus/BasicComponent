d47f89ddb2696ed7d0b8c1b439637e0ac25b6907
徐乔伦 <xql@feiniubus.com>
2017-10-18 21:10:07 +0800

    提供默认JSON序列化设置

--- src/FeiniuBus.AspNetCore.Json/FeiniuBusJsonConvert.cs
+++ src/FeiniuBus.AspNetCore.Json/FeiniuBusJsonConvert.cs
@@ -6,26 +6,9 @@ namespace FeiniuBus.AspNetCore.Json
 {
     public static class FeiniuBusJsonConvert
     {
-        private static readonly JsonSerializerSettings Settings;
-
-        static FeiniuBusJsonConvert()
-        {
-            Settings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                DateFormatHandling = DateFormatHandling.IsoDateFormat,
-                DateFormatString = "yyyy'-'MM'-'dd' 'HH':'mm':'ss",
-                MissingMemberHandling = MissingMemberHandling.Ignore
-            };
-            
-            Settings.Converters.Add(new StringEnumConverter());
-            Settings.Converters.Add(new IdConverter());
-        }
-
         public static string SerializeObject(object value)
         {
-            return JsonConvert.SerializeObject(value, Settings);
+            return JsonConvert.SerializeObject(value, FeiniuBusJsonSerializerSettings.Default);
         }
 
         public static string SerializeObject(object value, JsonSerializerSettings settings)
@@ -35,7 +18,7 @@ namespace FeiniuBus.AspNetCore.Json
 
         public static T DeserializeObject<T>(string value)
         {
-            return JsonConvert.DeserializeObject<T>(value);
+            return JsonConvert.DeserializeObject<T>(value, FeiniuBusJsonSerializerSettings.Default);
         }
 
         public static T DeserializeObject<T>(string value, JsonSerializerSettings settings)
---
+++ src/FeiniuBus.AspNetCore.Json/FeiniuBusJsonSerializerSettings.cs
@@ -0,0 +1,26 @@
+﻿using FeiniuBus.AspNetCore.Json.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FeiniuBus.AspNetCore.Json
+{
+    public static class FeiniuBusJsonSerializerSettings
+    {
+        static FeiniuBusJsonSerializerSettings()
+        {
+            Default = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateFormatString = "yyyy'-'MM'-'dd' 'HH':'mm':'ss",
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+            
+            Default.Converters.Add(new StringEnumConverter());
+            Default.Converters.Add(new IdConverter());
+        }
+        
+        public static JsonSerializerSettings Default { get; }
+    }
+}
