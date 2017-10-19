781671c490e88a7cbcdd42d1236948a67f4a46a8
徐乔伦 <xql@feiniubus.com>
2017-10-19 11:15:04 +0800

    add NLog

---
+++ src/FeiniuBus.AspNetCore.NLog/NLogExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using NLog.LayoutRenderers;
+using NLog.Web;
+using NLog.Web.LayoutRenderers;
+
+namespace FeiniuBus.AspNetCore.NLog
+{
+    public static class NLogExtensions
+    {
+        public static IWebHostBuilder UseFeiniuBusNLog(this IWebHostBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            
+            LayoutRenderer.Register<AspNetMvcActionRenderer>("aspnet-mvc-action");
+            LayoutRenderer.Register<AspNetMvcControllerRenderer>("aspnet-mvc-controller");
+            LayoutRenderer.Register<AspNetRequestHostLayoutRenderer>("aspnet-request-host");
+            LayoutRenderer.Register<AspNetRequestHttpMethodRenderer>("aspnet-request-method");
+            LayoutRenderer.Register<AspNetQueryStringLayoutRenderer>("aspnet-request-querystring");
+            LayoutRenderer.Register<AspNetRequestUserAgent>("aspnet-request-useragent");
+            LayoutRenderer.Register<AspNetRequestUrlRenderer>("aspnet-request-url");
+            LayoutRenderer.Register<AspNetUserIdentityLayoutRenderer>("aspnet-user-identity");
+            LayoutRenderer.Register<AspNetRequestIpLayoutRenderer>("aspnet-request-ip");
+            
+            return builder.UseNLog();
+        }
+    }
+}
---
+++ src/FeiniuBus.AspNetCore.NLog/NLogSharedBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using NLog;
+using NLog.Web;
+
+namespace FeiniuBus.AspNetCore.NLog
+{
+    public static class NLogSharedBuilder
+    {
+        private const string DefaultNLogConfigFile = "nlog.config";
+        private static readonly string[] PotentialEnvironmentPaths = {"HOME", "USERPROFILE"};
+        private const string DefaultSharedFileLocation = ".feiniubus";
+
+        public static LogFactory ConfigureNLog(string configFile = DefaultNLogConfigFile)
+        {
+            if (string.IsNullOrEmpty(configFile))
+            {
+                throw new ArgumentNullException(nameof(configFile));
+            }
+
+            var filePath = ResolveSharedFileLocation(configFile);
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new FileNotFoundException("File not exists.", filePath);
+            }
+
+            return NLogBuilder.ConfigureNLog(filePath);
+        }
+
+        private static string ResolveSharedFileLocation(string filename)
+        {
+            foreach (var env in PotentialEnvironmentPaths)
+            {
+                var envPath = Environment.GetEnvironmentVariable(env);
+                if (!string.IsNullOrEmpty(envPath))
+                {
+                    var filePath = TestSharedFileExists(Path.Combine(envPath, DefaultSharedFileLocation, filename));
+                    if (!string.IsNullOrEmpty(filePath))
+                    {
+                        return filePath;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static string TestSharedFileExists(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (File.Exists(path))
+                {
+                    return path;
+                }
+            }
+
+            return null;
+        }
+    }
+}
