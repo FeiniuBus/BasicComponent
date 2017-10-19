8962350fa18f44704125a098ad2c92f89aa391aa
徐乔伦 <xql@feiniubus.com>
2017-10-19 11:35:02 +0800

    add http request logging middleware

---
+++ src/FeiniuBus.AspNetCore.Logging/HttpLoggingMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace FeiniuBus.AspNetCore.Logging
+{
+    public class HttpLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public HttpLoggingMiddleware(RequestDelegate next, ILogger<HttpLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(context.Request.ContentType))
+            {
+                if (_logger != null && _logger.IsEnabled(LogLevel.Information))
+                {
+                    var builder = new StringBuilder();
+                    var uri = context.Request.GetDisplayUrl();
+                    builder.AppendFormat("RequestUri: {0}{1}", uri, Environment.NewLine);
+                    builder.AppendFormat("HttpMethod: {0}", context.Request.Method);
+
+                    if (context.Request.QueryString.HasValue)
+                    {
+                        builder.AppendFormat("{1}QueryString: {0}", context.Request.QueryString.ToUriComponent(),
+                            Environment.NewLine);
+                    }
+
+                    if (MayContainRequestBody(context.Request))
+                    {
+                        if (context.Request.Body.CanSeek)
+                        {
+                            var reader = new StreamReader(context.Request.Body);
+                            var body = await reader.ReadToEndAsync().ConfigureAwait(false);
+                            builder.AppendFormat("{1}RequestBody: {0}", body, Environment.NewLine);
+                            context.Request.Body.Seek(0, SeekOrigin.Begin);
+                        }
+                    }
+                    
+                    _logger.LogInformation(builder.ToString());
+                }
+            }
+
+            await _next(context);
+        }
+        
+        private static bool MayContainRequestBody(HttpRequest request)
+        {
+            return request.Method == "POST" || request.Method == "PUT" || request.Method == "PATCH";
+        }
+    }
+}
---
+++ src/FeiniuBus.AspNetCore.Logging/MultipartRequestHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FeiniuBus.AspNetCore.Logging
+{
+    internal static class MultipartRequestHelper
+    {
+        public static bool IsMultipartContentType(string contentType)
+        {
+            return !string.IsNullOrEmpty(contentType) &&
+                   contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+}
